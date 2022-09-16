7f0ef5992b978b7cec36fe94ee7eddc415570aac
Emmanuel Ngoka <emmanuelngoka778@gmail.com>
2022-09-16 03:26:18 +0100

    styled statistics

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -8,7 +8,7 @@ const Home = ()=>{
 	const {userInfo} = useAuth()
 	return (
 		<div className="px-5 dark:text-slate-200">
-			<div><h4 className="text-2xl">Hello {userInfo?.name?.split(" ")[0]}</h4></div>
+			<div><h4 className="text-xl font-medium text-slate-600">Hello {userInfo?.name?.split(" ")[0]}</h4></div>
 			<div>
 				<Statistics />
 			</div>
--- src/components/home/Statistics.tsx
+++ src/components/home/Statistics.tsx
@@ -20,13 +20,14 @@ const Statistics = ()=>{
     for(let i = 0; i <=6; i ++){
     	week.push(new Date(date.setDate(first + i)).toDateString())
     }
-    const weekStats = week.map(item => ({name: item[0], number: completed?.filter(i => new Date(i.date).toDateString() === item).length}))
+    const weekStats = week.map(item => ({name: item.split(' ')[0].toUpperCase(), number: completed?.filter(i => new Date(i.date).toDateString() === item).length}))
   return (
-    <motion.div className="text-white bg-gradient-to-br from-purple-500 w-56 h-56 to-purple-600 rounded-xl p-3">
+    <motion.div className="text-white bg-gradient-to-br from-purple-400 shadow-xl w-full h-36 to-purple-600 rounded-xl p-3">
       <ResponsiveContainer width="100%" height="100%">
         <LineChart width={300} height={400} data={weekStats}>
-          <Line type='monotone' dataKey="number" />
-          <XAxis tick={{fill: 'white'}} className="!text-white" dataKey="name"/>
+          <Line type='monotone' strokeWidth={2} stroke="#fff" dot={false} dataKey="number" />
+          <XAxis axisLine={false} interval="preserveStartEnd" tickLine={false} height={20} tick={{fill: 'white', fontSize: 12}} className="!text-white" dataKey="name"/>
+          <YAxis hide />
         </LineChart>
       </ResponsiveContainer>
     </motion.div>
