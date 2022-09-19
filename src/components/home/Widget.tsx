8515f4e4a3eeb5dd15d6fc367790326cd609ab1d
Emmanuel Ngoka <emmanuelngoka778@gmail.com>
2022-09-19 21:13:39 +0100

    fixed design flaws

--- src/components/home/Widget.tsx
+++ src/components/home/Widget.tsx
@@ -19,11 +19,11 @@ const variants = {
 function TodoWidget(){
 	const {userInfo} = useAuth()
 	return(
-		<Link to="/todo"><motion.div whileTap={{scale: 0.85}} className="text-white dark:text-white bg-violet-600 rounded-xl p-4">
+		<Link to="/todo"><motion.div whileTap={{scale: 0.85}} className="text-white dark:text-white bg-violet-500 rounded-xl p-4">
 			<div className="flex"><h4 className="font-medium text-2xl">Tasks</h4>
 				<Button text={<TbChevronRight fontSize={25} />} className="p-1 bg-white/25 rounded-lg ml-auto" />
 			</div>
-			<p className="text-sm text-slate-200 mt-2 w-32">You have tasks today</p>
+			<p className="text-sm text-slate-200 mt-2">You have tasks today</p>
 			
 		</motion.div>
 		</Link>
@@ -32,11 +32,11 @@ function TodoWidget(){
 
 function EventWidget() {
 	return (
-		<Link to="/events"><motion.div whileTap={{scale: 0.85}} className="text-white dark:text-white ml-auto bg-purple-500 dark:bg-[#332f6c] rounded-xl p-4">
+		<Link to="/events"><motion.div whileTap={{scale: 0.85}} className="text-white dark:text-white bg-purple-500 dark:bg-[#332f6c] rounded-xl p-4">
 			<div className="flex"><h4 className="font-medium text-2xl">Events</h4>
 				<Button text={<TbChevronRight fontSize={25} />} className="p-1 bg-white/25 rounded-lg ml-auto" />
 			</div>
-			<p className="text-sm text-slate-200 mt-2 w-32">You have events today</p>
+			<p className="text-sm text-slate-200 mt-2">You have events today</p>
 			
 		</motion.div>
 		</Link>
@@ -45,7 +45,7 @@ function EventWidget() {
 
 const Widget = () => {
 	return (
-		<motion.div variants={variants} initial="hidden" animate="visible" className="flex justify-between mt-7">
+		<motion.div variants={variants} initial="hidden" animate="visible" className="flex justify-between gap-6 mt-7">
 			<TodoWidget />
 			<EventWidget />
 		</motion.div>
